034fb4de877e483b0e62bbb942d47b5838fb05d3
emifa <emifa@LAPTOP-1TM449QB>
2023-07-02 15:18:07 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-8-Data_Coll-.NET/OperationWithNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_8_Data_Coll_.NET
+{
+    internal class OperationWithNumbers
+    {
+        public List<int> GenerateRandomNumbers(int count)
+        {
+            Random random = new Random();   
+            List<int> numbers = new List<int>();
+            for(int i = 0;i < count; i++ ){
+
+                int randomNumber = random.Next();
+                numbers.Add(randomNumber);
+                Console.WriteLine(numbers[i]);
+            }
+            
+            return numbers;
+        }
+
+        public int MaxNumber(List<int> numbers)
+        {
+            int max = int.MinValue;
+            for(int i =0;i < numbers.Count; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+            return max;
+        }
+
+        public int MinNumber(List<int> numbers) 
+        {
+            int min = int.MaxValue;
+            for(int i = 0;i < numbers.Count;i++)
+            { 
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+               
+            }
+            return min;
+        }
+    }
+}
---
+++ Esercizio-8-Data_Coll-.NET/Program.cs
@@ -0,0 +1,21 @@
+﻿using Esercizio_8_Data_Coll_.NET;
+
+class Program
+{
+   static void Main()
+    {
+        OperationWithNumbers operationWithNumbers = new OperationWithNumbers();
+        List<int> randomNumb = operationWithNumbers.GenerateRandomNumbers(1000);
+        
+        int min = operationWithNumbers.MinNumber(randomNumb);
+        int max = operationWithNumbers.MaxNumber(randomNumb);
+        Console.WriteLine();
+        Console.WriteLine("Minum number : " + min);
+        Console.WriteLine();
+        Console.WriteLine("Maximum number : " + max);
+    
+    }
+
+
+
+}
